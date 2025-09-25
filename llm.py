2dcef856dfcd1b05d97f1a9f230610597bac8a4b
namuwasae <aiscout2000@gmail.com>
2025-09-25 22:33:41 +0900

    feat(llm.py): qa_chain에서 history를 기억하는 rag_chain으로 변경

--- llm.py
+++ llm.py
@@ -1,13 +1,33 @@
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import ChatOpenAI
-from langchain import hub
 # query -> 직장인 -> 거주자로 바꾸는 chain을 추가를 위한 라이브러리
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import RetrievalQA
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from pinecone import Pinecone
+from langchain.chains import create_history_aware_retriever
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.memory import ChatMessageHistory
+from langchain.schema.runnable import RunnableWithMessageHistory
+from langchain.schema.messages import BaseChatMessageHistory
 
+
+### Statefully manage chat history ###
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+
+# 함수 이름 : get_llm
+# 함수 기능 : LLM 초기화하는 함수
+# 함수 파라미터 : model
+# 함수 반환값 : llm
 def get_llm(model = "gpt-4o-mini"):
     llm = ChatOpenAI(model = model, temperature = 0)
     return llm
@@ -31,6 +51,7 @@ def get_dictionary():
     dictionary_chain = dictionary_prompt | llm | StrOutputParser() 
     return dictionary_chain
 
+
 # 함수 이름 : get_retriever
 # 함수 기능 : 질문을 임베딩해 벡터로 만들고 이를 벡터스토어에 래핑하고 유사도를 비교해 유사도가 높은 상위 k개의 문서 청크를 가져오는 함수
 # 함수 파라미터 : 없음
@@ -49,17 +70,71 @@ def get_retriever():
 # 함수 기능 : RetrievalQA 체인 생성하는 함수
 # 함수 파라미터 : 없음
 # 함수 반환값 : qa_chain
-def get_qa_chain():
-    rag_prompt = hub.pull("rlm/rag-prompt")
+def get_rag_chain():
+    
     llm=get_llm()
     retriever = get_retriever()
-     # RetrievalQA 체인 생성
-    qa_chain = RetrievalQA.from_chain_type(
-        llm,
-        retriever=retriever,
-        chain_type_kwargs={"prompt": rag_prompt}
+
+# 시스템 프롬프트를 이용해 새로운 채팅 프롬프트를 만들 것임.
+    contextualize_q_system_prompt = (
+        "Given a chat history and the latest user question "
+        "which might reference context in the chat history, "
+        "formulate a standalone question which can be understood "
+        "without the chat history. Do NOT answer the question, "
+        "just reformulate it if needed and otherwise return it as is."
     )
-    return qa_chain
+
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    # 이 history_aware_retriever를 이용해서 
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+    )
+    
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+    # rag_chain을 받아서 conversational_rag_chain을 만듦
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+
+    return conversational_rag_chain
+
+
+
+    # # RetrievalQA 체인 생성
+    # qa_chain = RetrievalQA.from_chain_type(
+    #     llm,
+    #     retriever=retriever,
+    #     chain_type_kwargs={"prompt": rag_prompt}
+    # )
 
 
 # 함수 이름 : get_ai_message
@@ -70,9 +145,10 @@ def get_ai_message(user_message):
     llm = get_llm()
     dictionary_chain = get_dictionary()
     retriever = get_retriever()
-    qa_chain = get_qa_chain()
+    qa_chain = get_rag_chain()
 
     tax_chain = {"query":dictionary_chain} | qa_chain
     ai_message = tax_chain.invoke({"question": user_message})
 
     return ai_message["result"]
+
